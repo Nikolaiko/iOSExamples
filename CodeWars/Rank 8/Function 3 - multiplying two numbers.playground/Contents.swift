7dc501d33439da120c6e6938cf218896d7b9ba07
Nikolai Baklanov <nikolaiko@mail.ru>
2022-08-19 18:01:23 +0700

    Task

---
+++ CodeWars/Rank 8/Function 3 - multiplying two numbers.playground/Contents.swift
@@ -0,0 +1,15 @@
+/*
+ Implement a function called multiply, which takes two numbers and returns their product:
+
+ multiply(2, 3) = 6
+ multiply(0, 188) = 0
+ multiply(85, 144) = 12240
+ */
+
+import UIKit
+
+func multiply(_ a: Double, _ b: Double) -> Double {
+    return a * b;
+}
+
+print(multiply(5, 4))
