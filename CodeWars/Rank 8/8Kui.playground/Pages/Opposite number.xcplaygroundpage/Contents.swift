3065594c4c33ddceb51719c9e29a8c22b5e86ece
Nikolai Baklanov <nikolaiko@mail.ru>
2022-10-03 09:08:25 +0600

    small tasks

---
+++ CodeWars/Rank 8/8Kui.playground/Pages/Opposite number.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+/*
+
+ Very simple, given an integer or a floating-point number, find its opposite.
+
+ */
+
+func opposite(_ number: Double) -> Double {
+  -1 * number
+}
+
+print(opposite(14))
+print(opposite(1))
+print(opposite(-14))
