ebd9699d4ddd41857202fdeafa6ee4981eabe6cb
Nikolai Baklanov <n.baklanov@vk.team>
2023-11-02 10:43:58 +0600

    Another 8-kuy tasks

---
+++ CodeWars/Rank 8/8Kui.playground/Pages/Third Angle of a Triangle.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+/*
+ You are given two interior angles (in degrees) of a triangle.
+
+ Write a function to return the 3rd.
+
+ Note: only positive integers will be tested.
+ */
+
+func otherAngle(a: Int, b: Int) -> Int {
+  return 180 - a - b
+}
